9e30f69eead6431f5b0f9b01c82c951b4bdf64bf
Kaamin <ClarkJackson9@gmail.com>
2015-02-04 20:21:41 -0500

    Formatting
    Fixed PIDSpeedController formatting

--- RecycleRush/src/org/usfirst/frc2051/robot/RecycleRush/lib/PIDSpeedController.java
+++ RecycleRush/src/org/usfirst/frc2051/robot/RecycleRush/lib/PIDSpeedController.java
@@ -4,43 +4,51 @@ import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
 
-public class PIDSpeedController implements SpeedController {
-	
+public class PIDSpeedController implements SpeedController
+{
+
 	PIDController pid;
 	double Kp;
 	double Ki;
 	double Kd;
 
-	public PIDSpeedController(SpeedController controller, double Kp, double Ki, double Kd, PIDSource source) {
+	public PIDSpeedController(SpeedController controller, double Kp, double Ki,
+			double Kd, PIDSource source)
+	{
 		this.Kp = Kp;
 		this.Ki = Ki;
 		this.Kd = Kd;
 		pid = new PIDController(Kp, Ki, Kd, source, controller);
 		pid.enable();
 	}
-	
+
 	@Override
-	public void pidWrite(double output) {
-		pid.setSetpoint(output/Kp);
+	public void pidWrite(double output)
+	{
+		pid.setSetpoint(output / Kp);
 	}
 
 	@Override
-	public double get() {
-		return (pid.getSetpoint()*Kp);
+	public double get()
+	{
+		return (pid.getSetpoint() * Kp);
 	}
 
 	@Override
-	public void set(double speed, byte syncGroup) {
+	public void set(double speed, byte syncGroup)
+	{
 		pidWrite(speed);
 	}
 
 	@Override
-	public void set(double speed) {
+	public void set(double speed)
+	{
 		pidWrite(speed);
 	}
 
 	@Override
-	public void disable() {
+	public void disable()
+	{
 		pid.disable();
 	}
 
