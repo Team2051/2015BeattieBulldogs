dbd58b973174b57c124e96e1e1732588e86d8fb5
Kaamin <ClarkJackson9@gmail.com>
2015-03-07 09:53:18 -0500

    Removed Pot Safety for Lifters
    Potentiomiters are still providing incorrect values

--- RecycleRush/src/org/usfirst/frc2051/RecycleRush/commands/LifterLeftDown.java
+++ RecycleRush/src/org/usfirst/frc2051/RecycleRush/commands/LifterLeftDown.java
@@ -23,7 +23,7 @@ public class LifterLeftDown extends Command
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute()
 	{		
-		if(Robot.lifterPIDLeft.getPosition() > RobotMap.LIFTER_MIN)
+//		if(Robot.lifterPIDLeft.getPosition() > RobotMap.LIFTER_MIN)
 			Robot.lifterLeft.down();
 	}
 
--- RecycleRush/src/org/usfirst/frc2051/RecycleRush/commands/LifterLeftUp.java
+++ RecycleRush/src/org/usfirst/frc2051/RecycleRush/commands/LifterLeftUp.java
@@ -23,7 +23,7 @@ public class LifterLeftUp extends Command
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute()
 	{
-		if(Robot.lifterPIDLeft.getPosition() < RobotMap.LIFTER_MAX)
+//		if(Robot.lifterPIDLeft.getPosition() < RobotMap.LIFTER_MAX)
 			Robot.lifterLeft.up();
 	}
 
